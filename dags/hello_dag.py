b2563f95be2df48860d7160c6654710dbcf2bd3a
Amir380-A <62997533+Amir380-A@users.noreply.github.com>
2025-05-26 22:16:38 +0300

    add first dag

---
+++ dags/hello_dag.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def say_hello():
+    print("👋 Hello from Airflow!")
+
+with DAG(
+    dag_id="hello_airflow",
+    start_date=datetime(2025, 5, 25),
+    schedule_interval="@daily",  # Runs daily
+    catchup=True,
+    tags=["example"],
+) as dag:
+
+    hello_task = PythonOperator(
+        task_id="say_hello",
+        python_callable=say_hello
+    )
+
